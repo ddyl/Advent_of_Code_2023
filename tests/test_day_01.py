32a515b8db5b29ee8aab0311aafb838e17703d95
Daniel <dlee.coding@gmail.com>
2023-12-03 12:58:19 -0700

    Add test for day 1

--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -1,7 +1,18 @@
-from advent_of_code_2023.day_01.solution import main
+from pathlib import Path
+import os
 
-def test_main_false():
-    assert main() == 0
+from advent_of_code_2023.day_01 import trebuchet
 
-def test_main_true():
-    assert main() == 1
+def test_solution_1():
+    
+    test_input_path = os.path.join(Path(__file__).parent, "test_inputs", "day_01_test_1.txt")
+
+    with open(test_input_path) as input_file:
+        assert trebuchet(input_file.read().splitlines(), digit_only=True) == 142
+
+def test_solution_2():
+    
+    test_input_path = os.path.join(Path(__file__).parent, "test_inputs", "day_01_test_2.txt")
+
+    with open(test_input_path) as input_file:
+        assert trebuchet(input_file.read().splitlines(), digit_only=False) == 281
